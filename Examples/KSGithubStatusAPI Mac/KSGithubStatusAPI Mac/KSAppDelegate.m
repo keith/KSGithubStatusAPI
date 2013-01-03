e5afd516fd578c9e16717775a4d40c89c6cbee8c
Keith Smiley <keithbsmiley@gmail.com>
2013-01-03 15:40:19 -0500

    Updated mac example

--- Examples/KSGithubStatusAPI Mac/KSGithubStatusAPI Mac/KSAppDelegate.m
+++ Examples/KSGithubStatusAPI Mac/KSGithubStatusAPI Mac/KSAppDelegate.m
@@ -7,12 +7,24 @@
 //
 
 #import "KSAppDelegate.h"
+#import "KSGithubStatusAPI.h"
 
 @implementation KSAppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-    
+    [[KSGithubStatusAPI sharedClient] checkStatus:^(NSNumber *available, NSError *error) {
+        if ([available boolValue]) {
+            NSLog(@"Github is available");
+        } else {
+            NSLog(@"Github isn't available");
+        }
+        
+        NSLog(@"Details: %@", [[KSGithubStatusAPI sharedClient] currentStatusDetails]);
+        
+        NSLog(@"Last Checked: %@", [[KSGithubStatusAPI sharedClient] lastCheckedDate]);
+        NSLog(@"Last Checked Pretty: %@", [[KSGithubStatusAPI sharedClient] lastCheckedPrettyDate]);
+    }];
 }
 
 @end
