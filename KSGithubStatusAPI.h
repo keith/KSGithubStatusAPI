8f6629e1bada3a112f62490a671283075914cf97
Keith Smiley <keithbsmiley@gmail.com>
2013-01-03 13:16:40 -0500

    Added api files

---
+++ KSGithubStatusAPI.h
@@ -0,0 +1,13 @@
+//
+//  KSGithubStatusAPI.h
+//  KSGithubStatusAPI Mac
+//
+//  Created by Keith Smiley on 1/3/13.
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KSGithubStatusAPI : NSObject
+
+@end
---
+++ KSGithubStatusAPI.m
@@ -0,0 +1,13 @@
+//
+//  KSGithubStatusAPI.m
+//  KSGithubStatusAPI Mac
+//
+//  Created by Keith Smiley on 1/3/13.
+//  Copyright (c) 2013 Keith Smiley. All rights reserved.
+//
+
+#import "KSGithubStatusAPI.h"
+
+@implementation KSGithubStatusAPI
+
+@end
